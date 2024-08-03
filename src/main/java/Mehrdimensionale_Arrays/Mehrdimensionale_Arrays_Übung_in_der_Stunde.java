740ca81f3ed38f26e64ff04a6ed2f539b5ebc3e6
Markous <markous124@gmail.com>
2024-08-04 00:25:02 +0200

    1. Commit 03.08.2024 (Einfügen Schulübung)

--- src/main/java/Mehrdimensionale_Arrays/Mehrdimensionale_Arrays_Übung_in_der_Stunde.java
+++ src/main/java/Mehrdimensionale_Arrays/Mehrdimensionale_Arrays_Übung_in_der_Stunde.java
@@ -2,6 +2,39 @@ package Mehrdimensionale_Arrays;
 
 public class Mehrdimensionale_Arrays_Übung_in_der_Stunde {
     public static void main(String[] args) {
+//---------------------------------------------------------- Verschachtelte Schleifen und 2D Arrays
+        System.out.println("Matrix: ");
+        String[][] ausgabe = new String[5][6];
+        //Verarbeitung Zeilenindex
+        for (int i1 = 0; i1 < 5; i1++)
+        {
+            //Verarbeitung Spaltenindex
+            for (int i2 = 0; i2 < 5; i2++)
+            {
+                ausgabe[i1][i2] = "x";
+            }
+            ausgabe[i1][5] = "\n";
+        }
+
+
+        for (int i1 = 0; i1 < 5; i1++)
+        {
+            //Verarbeitung Spaltenindex
+            for (int i2 = 0; i2 < 5; i2++)
+            {
+                System.out.print(ausgabe[i1][i2]);
+            }
+            System.out.print(ausgabe[i1][5]);
+        }
+
+
+        //Exkurs 2D Foreach
+        /*for (String[] zeile: ausgabe) {
+            for (String spalte: zeile) {
+                System.out.print(spalte);
+            }
+        }*/
+
 
     }
 }
--- src/main/java/Switch_Case/Switch_Case_Übung_in_der_Stunde.java
+++ src/main/java/Switch_Case/Switch_Case_Übung_in_der_Stunde.java
@@ -2,6 +2,55 @@ package Switch_Case;
 
 public class Switch_Case_Übung_in_der_Stunde {
     public static void main(String[] args) {
+        //Bisher
+        int iZahl1 = 7;
+        if(iZahl1 == 5)
+        {
+            System.out.println("Die Zahl ist 5");
+        }
+        else if(iZahl1 == 6)
+        {
+            System.out.println("Die Zahl ist 6");
+        }
+        else if(iZahl1 != 6)
+        {
+            System.out.println("Die Zahl ist nicht 6");
+        }
 
+        //Idee Switch case Idee = übersicht über Verzweigungen Schaffen
+        //Create, Delete, Update, Insert,.....
+        String sKommando = "Create";
+        if (sKommando == "Create")
+        {
+            System.out.println("Erstellen");
+        }
+        else if(sKommando == "Delete")
+        {
+            System.out.println("Löschen");
+        }
+        else if(sKommando == "Update")
+        {
+            System.out.println("Ändern");
+        }
+        else if(sKommando == "Insert")
+        {
+            System.out.println("Einfügen");
+        }
+
+        switch (sKommando)
+        {
+            case "Create":
+                System.out.println("Erstellen");
+                break;
+            case "Update":
+                System.out.println("Ändern");
+                break;
+            case "Insert":
+                System.out.println("Einfügen");
+                break;
+            default:
+                System.out.println("kein richtiges Kommando");
+                break;
+        }
     }
 }
