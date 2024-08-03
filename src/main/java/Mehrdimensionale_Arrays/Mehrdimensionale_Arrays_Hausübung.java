9ca83af4574dcd72c3295244b2f22bf201afadb4
Markous <markous124@gmail.com>
2024-08-04 00:35:48 +0200

    1. Commit 03.08.2024 (Einfügen und Definition Hausübung)

---
+++ src/main/java/Mehrdimensionale_Arrays/Mehrdimensionale_Arrays_Hausübung.java
@@ -0,0 +1,14 @@
+package Mehrdimensionale_Arrays;
+/*
+Befülle ein 2D Array Integerarray mit einer 5 x 5 Matrix mit zufälligen Werten von (1 - 10)
+
+Diese sollen dannach alle Zusammengezählt werden.
+
+Die Errechnete Summe und Matrix soll in der Konsole zu guter Letzt ausgegeben werden.
+
+ */
+public class Mehrdimensionale_Arrays_Hausübung {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/Switch_Case/Switch_Case_Hausübung.java
@@ -0,0 +1,22 @@
+package Switch_Case;
+
+/*Lies per Scanner 2 Zahlen und ein Rechenzeichen (+, -, *, /) ein
+* Sorge dafür dass mit den 2 Zahlen die eingegebene Rechenoperation durchgeführt wird
+* Achte auf entsprechene Fehlermelungen wenn (/0 dividiert wird, ein ungültiges rechenzeichen eingegeben wird,...)
+*
+* */
+/*
+Befülle ein 2D Array Integerarray mit einer 5 x 5 Matrix mit zufälligen Werten von (1 - 10)
+
+Diese sollen dannach alle Zusammengezählt werden.
+
+Die Errechnete Summe und Matrix soll in der Konsole zu guter Letzt ausgegeben werden.
+
+ */
+
+
+public class Switch_Case_Hausübung {
+    public static void main(String[] args) {
+
+    }
+}
