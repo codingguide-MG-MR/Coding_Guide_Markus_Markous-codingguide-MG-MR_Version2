876f5d70964a68151d86360cae579763ba2e975a
markusrizkalla <rizkallamarkus245@gmail.com>
2024-08-04 19:00:43 +0300

    3. Commit 04.08.2024 (Kommentar aus Code entfernt, weil doppelt)

--- src/main/java/Switch_Case/Switch_Case_Hausübung.java
+++ src/main/java/Switch_Case/Switch_Case_Hausübung.java
@@ -5,14 +5,7 @@ package Switch_Case;
 * Achte auf entsprechene Fehlermelungen wenn (/0 dividiert wird, ein ungültiges rechenzeichen eingegeben wird,...)
 *
 * */
-/*
-Befülle ein 2D Array Integerarray mit einer 5 x 5 Matrix mit zufälligen Werten von (1 - 10)
 
-Diese sollen dannach alle Zusammengezählt werden.
-
-Die Errechnete Summe und Matrix soll in der Konsole zu guter Letzt ausgegeben werden.
-
- */
 
 
 public class Switch_Case_Hausübung {
