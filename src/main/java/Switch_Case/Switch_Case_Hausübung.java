36021020ae30872973bdbf14af67f6a0328b2ea5
markusrizkalla <rizkallamarkus245@gmail.com>
2024-08-31 17:09:07 +0200

    4. Commit in Stunde Korrektur vom 31.08.2024

--- src/main/java/Switch_Case/Switch_Case_Hausübung.java
+++ src/main/java/Switch_Case/Switch_Case_Hausübung.java
@@ -7,9 +7,64 @@ package Switch_Case;
 * */
 
 
+import java.util.Scanner;
 
 public class Switch_Case_Hausübung {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Geben Sie Ihre 1. Zahl ein: ");
+        Double number = scanner.nextDouble();
+
+
+
+        System.out.println("Geben Sie Ihre 2. Zahl ein: ");
+        Double number2 = scanner.nextDouble();
+
+
+        System.out.println("Geben Sie Ihr Rechenzeichen ein: ");
+        char number3 = scanner.next().charAt(0);
+
+        Double ergebnis = null;
+
+        switch (number3){
+            case '+':
+                ergebnis = number + number2;
+                System.out.println("Ergebnis: " + number + " + " + number2 + " = " + ergebnis);
+                break;
+
+            case '-':
+                ergebnis = number - number2;
+                System.out.println("Ergebnis: " + number + " - " + number2 + " = " + ergebnis);
+                break;
+
+            case '*':
+                ergebnis = number * number2;
+                System.out.println("Ergebnis: " + number + " * " + number2 + " = " + ergebnis);
+                break;
+
+            case '/':
+
+                if (number2 == 0)
+                {
+                    System.out.println("Es darf nicht durch null (0) dividiert werden!");
+                }
+
+                ergebnis = number / number2;
+                System.out.println("Ergebnis: " + number + " / " + number2 + " = " + ergebnis);
+                break;
+            default:
+                System.out.println("Das ist kein gültiges Zeichen! ");
+                ergebnis = null;
+                break;
+        }
+
+
+
+
+
+        scanner.close();
+
 
     }
 }
